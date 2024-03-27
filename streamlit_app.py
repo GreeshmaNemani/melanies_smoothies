abd10e5e14447669dcb75a2f879dbcdc0fcb2a14
GreeshmaNemani <165128569+GreeshmaNemani@users.noreply.github.com>
2024-03-27 15:25:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -50,5 +52,4 @@ if ingredients_list:
 
 
 # New section to display fruityvice nutrition information
-import requests
 # st.text(fruityvice_response.json())
